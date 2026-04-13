14407bde62de0fcdee2d990d972e07795afbfd9f
Balti <gitbal17@gmail.com>
2026-04-13 14:49:37 -0300

    Ejercicio ej1-variables/ COMPLETADO

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,16 @@
+// 1. Declaro las variables constantes
+const nombre = "Baltazar Martinez"; 
+const edad = 21; 
+const materia = "Desarrollo de Software";
+
+// 2. Muestro el mensaje usando Template Literals
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+// 3. Declaro el contador con let (porque su valor va a cambiar)
+let contador = 0;
+
+// 4. Sumo 1 tres veces
+contador++;            
+
+// 5. Mostramos el valor final por consola
+console.log("El valor final del contador es:", contador);
